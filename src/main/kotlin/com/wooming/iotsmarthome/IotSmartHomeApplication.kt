b0aab3ec1493401dd3e342b2d03630cd09282491
wooming <a11873@A11873ui-MacBookPro.local>
2022-09-01 18:56:01 +0900

    Initial Commit

---
+++ src/main/kotlin/com/wooming/iotsmarthome/IotSmartHomeApplication.kt
@@ -0,0 +1,11 @@
+package com.wooming.iotsmarthome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IotSmartHomeApplication
+
+fun main(args: Array<String>) {
+    runApplication<IotSmartHomeApplication>(*args)
+}
---
+++ src/test/kotlin/com/wooming/iotsmarthome/IotSmartHomeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wooming.iotsmarthome
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IotSmartHomeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
